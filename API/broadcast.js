96cf0e28bcf4e07109f26694c24ddd1670117b97
astaful123gg-a11y <astaful123gg@gmail.com>
2026-07-16 22:04:41 +0600

    Delete API directory

--- API/broadcast.js
+++
@@ -1,6 +0,0 @@
-const data = require('../broadcast.json');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
-  res.json({ broadcast: data.active ? data : null });
-};
--- API/downloaders.js
+++
@@ -1,7 +0,0 @@
-const data = require('../downloaders.json');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
-  const active = (data.downloaders || []).filter(d => d.active !== false);
-  res.json({ downloaders: active });
-};
